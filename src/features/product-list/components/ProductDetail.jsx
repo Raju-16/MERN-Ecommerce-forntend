26c88582aebea359ed455cb01e87457d681b2d4d
Raju Prajapati <raju@byldd.com>
2024-01-10 10:46:12 +0530

    edit some css regarding product details page

--- src/features/product-list/components/ProductDetail.jsx
+++ src/features/product-list/components/ProductDetail.jsx
@@ -65,8 +65,8 @@ const ProductDetail = () => {
   const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
 
   return (
-    <div className="bg-white">
-      <div className="pt-6">
+    <div className="bg-white flex justify-center">
+      <div className="pt-6 max-w-6xl">
         <nav aria-label="Breadcrumb">
           <ol className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
             {product.breadcrumbs.map((breadcrumb) => (
