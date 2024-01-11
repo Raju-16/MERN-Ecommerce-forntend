53c1e7f00d451499b7d8d384a21633584d81921b
Raju Prajapati <raju@byldd.com>
2024-01-11 19:02:58 +0530

    some change in the css of product list

--- src/features/product/components/ProductList.jsx
+++ src/features/product/components/ProductList.jsx
@@ -475,7 +475,7 @@ const ProductList = () => {
                     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
                       {products.map((product) => (
                         <Link to="/product-detail" key={product.id}>
-                          <div className="group relative border-solid border-2 p-2 border-gray-200">
+                          <div className="group relative border-solid rounded-md border-2 p-3 pb-3 border-gray-200">
                             <div className="min-h-60 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-60">
                               <img
                                 src={product.thumbnail}
@@ -483,32 +483,28 @@ const ProductList = () => {
                                 className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                               />
                             </div>
-                            <div className="mt-4 flex justify-between">
-                              <div>
-                                <h3 className="text-sm text-gray-700">
-                                  <a href={product.thumbnail}>
-                                    <span
-                                      aria-hidden="true"
-                                      className="absolute inset-0"
-                                    />
-                                    {product.title}
-                                  </a>
+                            <div className="mt-2">
+                              <div className="mt-2 flex justify-between items-center">
+                                <h3 className="text-md font-medium text-gray-900">
+                                  {product.title}
                                 </h3>
                                 <p className="mt-1 text-sm text-gray-500">
-                                  <StarIcon className="w-6 h-6 inline"></StarIcon>
-                                  <span className=" align-bottom">
-                                    {product.rating}
+                                  <span className=" align-bottom ml-1">
+                                    $
+                                    {Math.round(
+                                      product.price *
+                                        (1 - product.discountPercentage / 100)
+                                    )}
                                   </span>
                                 </p>
                               </div>
-                              <div>
-                                <p className="text-sm block font-medium text-gray-900">
-                                  $
-                                  {Math.round(
-                                    product.price *
-                                      (1 - product.discountPercentage / 100)
-                                  )}
-                                </p>
+                              <div className="mt-1 flex justify-between items-center">
+                                <div className="flex items-center">
+                                  <StarIcon className="w-4 h-4 inline" />
+                                  <p className="text-sm block font-medium text-gray-900">
+                                    {product.rating}
+                                  </p>
+                                </div>
                                 <p className="text-sm block line-through font-medium text-gray-400">
                                   ${product.price}
                                 </p>
