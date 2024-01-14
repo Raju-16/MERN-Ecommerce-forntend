a7548566b2bbd3be34001cf2be21df02d959671f
Raju Prajapati <raju@byldd.com>
2024-01-14 13:13:26 +0530

    create private route and update login and signup page

--- src/pages/AppRouter.jsx
+++ src/pages/AppRouter.jsx
@@ -6,16 +6,45 @@ import LoginPage from "./LoginPage";
 import CartPage from "./CartPage";
 import Checkout from "./Checkout";
 import ProductDetailPage from "./ProductDetailPage";
+import Protected from "../features/auth/components/Protected";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <Protected>
+            <Home />
+          </Protected>
+        }
+      />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/product-detail/:id" element={<ProductDetailPage />} />
+      <Route
+        path="/cart"
+        element={
+          <Protected>
+            <CartPage />
+          </Protected>
+        }
+      />
+      <Route
+        path="/checkout"
+        element={
+          <Protected>
+            <Checkout />
+          </Protected>
+        }
+      />
+      <Route
+        path="/product-detail/:id"
+        element={
+          <Protected>
+            <ProductDetailPage />
+          </Protected>
+        }
+      />
     </Routes>
   );
 };
